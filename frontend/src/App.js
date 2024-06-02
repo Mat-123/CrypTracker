6c178f36a095c7dc1fe5d905faef89b41e821e00
Mat-123 <156803363+Mat-123@users.noreply.github.com>
2024-06-02 15:19:13 +0200

    app.js

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,6 +1,5 @@
 import logo from './logo.svg';
 import './App.css';
-<<<<<<< HEAD
 import { BrowserRouter } from 'react-router-dom';
 
 function App() {
@@ -8,27 +7,6 @@ function App() {
     <BrowserRouter>
 
     </BrowserRouter>
-=======
-
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
->>>>>>> main
   );
 }
 
